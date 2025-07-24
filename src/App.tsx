9b2ccadf89b1123419328633f6e0af7ba65d42ce
caio20538 <caio20538@gmail.com>
2025-07-24 17:16:05 -0300

    feat: update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { Header } from './components/Header/Header';
-import { ProductCards } from './components/ProductCards/ProductCards';
+import { ProductsList } from './components/ProductsList/ProductsList';
 
 
 
@@ -11,7 +11,7 @@ function App() {
     <>
       <Header />
 
-      <ProductCards />
+      <ProductsList />
       <GlobalStyles />
     </>
   );
