3776251eda08c8b6d71b0a0f1ae5b82fb8aceda5
caio20538 <caio20538@gmail.com>
2025-07-24 17:15:52 -0300

    feat: update header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 
-import { FiLogIn, FiShoppingCart } from 'react-icons/fi';
+import { FiLogIn, FiShoppingCart, FiLogOut } from 'react-icons/fi';
 
 import * as S from "./styles"
+import { Cart } from "../Cart/Cart";
 
 export const Header: React.FC = () => {
+    const [showCart, setShowCart] = useState(false);
+    const isLoged = false;
+
     return(
         <S.StyledHeader>
             <S.Wreapper>
                 <S.HeaderTitle>MyShop.</S.HeaderTitle>
 
                 <S.ButtonsWrapper>
-                    <S.AuthButton>
-                        Login
-                        <FiLogIn />
+                    <S.AuthButton isLoged= {isLoged}>
+                        {isLoged ? "Logout" : "Login"}
+                        {isLoged ? <FiLogOut /> : <FiLogIn />}
                     </S.AuthButton>
 
-                    <S.CartButton>
+                    <S.CartButton onClick={() => setShowCart(!showCart)}>
                         Carrinho
                         <FiShoppingCart />
                     </S.CartButton>
                 </S.ButtonsWrapper>
             </S.Wreapper>
+
+            <Cart showCart = {showCart}/>
         </S.StyledHeader>
     );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -24,12 +24,16 @@ export const ButtonsWrapper = styled.div`
     gap: 1rem;
 `;
 
-export const AuthButton = styled.button`
+interface AuthButtonProps{
+    isLoged: boolean
+}
+
+export const AuthButton = styled.button<AuthButtonProps>`
     border: none;
     border-radius: 5px;
     height: 30px;
     padding: 0 1rem;
-    background-color: green;
+    background-color: ${(props) => props.isLoged ? "red": "green"};
     color: white;
     font-size: 0.8rem;
 
