f3d28fab3fa74621463435df239c34af3e8ae841
caio20538 <caio20538@gmail.com>
2025-07-24 17:15:08 -0300

    feat: create product List component

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import * as S from "./style";
+import { ProductCards } from "../ProductCards/ProductCards";
+import {Products} from "../../data/Products"
 
 export const ProductsList:React.FC = () =>{
+    //pode usar um useEfect de uma API
+    
     return(
-        
+        <S.Container>
+            {Products.map((product) => {
+                return <ProductCards key={product.id} product={product}/>
+            }) }
+         
+        </S.Container>
     );
 }
--- src/components/ProductsList/style.ts
+++ src/components/ProductsList/style.ts
@@ -2,5 +2,21 @@ import styled from "styled-components";
 
 export const Container = styled.main`
     max-width: 1240px;
-    margin: 0 auto;
+    //isso e arrumando a altura do container deixa todas os cards na msm altura
+    height: 100%;
+    margin: 4rem auto;
+    padding: 0 2rem;
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    //joga os itens para o topo sem esticar
+    align-items: stretch;
+    gap: 2rem;
+
+    //assim diz que tudo abaixo ou dentro vai ter esse estilo   
+    & > article{
+        //estica o maximo tendo no minimo 300px senão quebra em conjunto ao flexwrap
+        flex: 1 300px;
+    }
 `;
